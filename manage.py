58f35b3b3c572fcc0c1a8ac3dfe20da336aac9d3
Nishith-Gohel <62199169+Nishith-Gohel@users.noreply.github.com>
2021-11-26 21:05:51 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -5,9 +5,6 @@ Command-line utility for administrative tasks.
 # For more information about this file, visit
 # https://docs.djangoproject.com/en/2.1/ref/django-admin/
 """
-import os
-import sys
-
 if __name__ == '__main__':
     os.environ.setdefault(
         'DJANGO_SETTINGS_MODULE',
